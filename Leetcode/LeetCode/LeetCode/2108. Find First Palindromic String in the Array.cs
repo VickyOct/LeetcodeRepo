02e54abdfd16545a4394a6c1211960dd09cfa08a
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-02-16 14:22:27 -0500

    First attemp of 2108

---
+++ Leetcode/LeetCode/LeetCode/2108. Find First Palindromic String in the Array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Solution
+{
+    public string FirstPalindrome(string[] words)
+    {
+        if (words == null)
+            return "";
+
+        foreach (string word in words)
+        {
+            char[] charArray = word.ToCharArray();
+            int center = (word.Length - 1) / 2;
+            int left = center - 1;
+            int right = center + 1;
+            while (left >= 0 && right < charArray.Length && charArray[left] == charArray[right])
+            {
+                left--;
+                right++;
+                if(left < 0)
+                    return word;
+            };
+        }
+        return "";
+    }
+}
