b4834d65245fe9378196e0021e8efde11e5552ed
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-02-16 14:30:36 -0500

    Fix: use the first char and the last char to check
    the first attempt could not solve when the word is one char and two char

--- Leetcode/LeetCode/LeetCode/2108. Find First Palindromic String in the Array.cs
+++ Leetcode/LeetCode/LeetCode/2108. Find First Palindromic String in the Array.cs
@@ -10,15 +10,14 @@ public class Solution
         foreach (string word in words)
         {
             char[] charArray = word.ToCharArray();
-            int center = (word.Length - 1) / 2;
-            int left = center - 1;
-            int right = center + 1;
-            while (left >= 0 && right < charArray.Length && charArray[left] == charArray[right])
+            int left = 0;
+            int right = word.Length - 1;
+            while (charArray[left] == charArray[right])
             {
-                left--;
-                right++;
-                if(left < 0)
+                if (left >= right)
                     return word;
+                left++;
+                right--;
             };
         }
         return "";
