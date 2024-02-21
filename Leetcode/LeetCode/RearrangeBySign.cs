8ee1fb8543f072ad8fb574b3b3008cf73935a866
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-02-21 15:52:58 -0500

    Create 2149 Rearrange Array Elements By sign

---
+++ Leetcode/LeetCode/RearrangeBySign.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int[] RearrangeArray(int[] nums)
+    {
+        int[] result = new int[nums.length];
+        int Pos = 0;
+        int Neg = 1;
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] > 0)
+            {
+                result[Pos] = nums[i];
+            }
+            else
+            {
+                result[++Neg] = nums[i];
+            }
+        }
+        return result;
+    }
+}
