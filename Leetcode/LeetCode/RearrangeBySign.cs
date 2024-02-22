ef53b2b4396384528543e71f179a5c894d6f037f
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-02-22 17:04:36 -0500

    Update RearrangeBySign

--- Leetcode/LeetCode/RearrangeBySign.cs
+++ Leetcode/LeetCode/RearrangeBySign.cs
@@ -2,7 +2,7 @@
 {
     public int[] RearrangeArray(int[] nums)
     {
-        int[] result = new int[nums.length];
+        int[] result = new int[nums.Length];
         int Pos = 0;
         int Neg = 1;
         for(int i = 0; i < nums.Length; i++)
@@ -10,10 +10,12 @@
             if(nums[i] > 0)
             {
                 result[Pos] = nums[i];
+                Pos += 2;
             }
             else
             {
-                result[++Neg] = nums[i];
+                result[Neg] = nums[i];
+                Neg += 2;
             }
         }
         return result;
