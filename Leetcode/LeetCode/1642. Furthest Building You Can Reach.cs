a2c8b78f28827241d77f51024963b5071977b650
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-02-23 15:56:32 -0500

    Create 1642. Furthest Building You Can Reach.cs
    Use priority queue to dequeue the minimal element

---
+++ Leetcode/LeetCode/1642. Furthest Building You Can Reach.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public int FurthestBuilding(int[] heights, int bricks, int ladders)
+    {
+        int bricksLeft = bricks;
+        int laddersLeft = ladders;
+        PriorityQueue<int, int> bricksUsed = new PriorityQueue<int, int>();
+
+        for (int i = 0; i < heights.Length - 1; i++)
+        {
+            int Hdif = heights[i + 1] - heights[i];
+            if (Hdif > 0)
+            {
+                bricksLeft -= Hdif;
+                bricksUsed.Enqueue(Hdif, -Hdif);
+            }
+
+            if (bricksLeft < 0)
+            {
+                if (laddersLeft > 0)
+                {
+                    bricksLeft += bricksUsed.Dequeue();
+                    laddersLeft--;
+                }
+                else
+                {
+                    return i;
+                }
+            }
+
+        }
+        return heights.Length - 1;
+
+    }
+}
