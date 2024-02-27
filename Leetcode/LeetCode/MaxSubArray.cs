f824cafb1499b0b159cec20a3531a44433443751
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-02-27 16:54:45 -0500

    Create MaxSubArray.cs

---
+++ Leetcode/LeetCode/MaxSubArray.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int MaxSubArray(int[] nums)
+    {
+        int curSum = 0;
+        int maxSum = nums[0];
+        foreach (int i in nums)
+        {
+            curSum = Math.Max(i, i + curSum);
+            maxSum = Math.Max(maxSum, curSum);
+        }
+        return maxSum;
+    }
+}
