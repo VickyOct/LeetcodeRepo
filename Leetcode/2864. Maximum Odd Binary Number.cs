f696e119aa249e6083476936705e5189d2a77e1e
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-01 11:01:39 -0500

    Create 2864. Maximum Odd Binary Number.cs

---
+++ Leetcode/2864. Maximum Odd Binary Number.cs
@@ -0,0 +1,49 @@
+﻿public class Solution
+{
+    public string MaximumOddBinaryNumber(string s)
+    {
+        Dictionary<int, int> numCount = new Dictionary<int, int>();
+        int[] nums = ToIntArray(s);
+
+        foreach (int i in nums)
+        {
+            if (numCount.ContainsKey(i))
+                numCount[i]++;
+            else
+                numCount[i] = 1;
+        }
+
+        string result = "";
+        if (numCount.ContainsKey(1) && numCount[1] > 1)
+        {
+            while (numCount[1] > 1)
+            {
+                result += "1";
+                numCount[1]--;
+            }
+        }
+
+        while (numCount.ContainsKey(0) && numCount[0] > 0)
+        {
+            result += "0";
+            numCount[0]--;
+        }
+
+        if (numCount.ContainsKey(1) && numCount[1] == 1)
+        {
+            result += "1";
+        }
+
+        return result;
+    }
+
+    private int[] ToIntArray(string s)
+    {
+        int[] nums = new int[s.Length];
+        for (int i = 0; i < s.Length; i++)
+        {
+            nums[i] = s[i] - '0'; // Convert char to int
+        }
+        return nums;
+    }
+}
