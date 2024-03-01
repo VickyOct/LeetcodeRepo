15be048db02d4625e4ff30068fd86584c91e223a
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-01 09:44:19 -0500

    Create 1609.EvenOddTree.cs

---
+++ Leetcode/LeetCode/1609.EvenOddTree.cs
@@ -0,0 +1,73 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public bool IsEvenOddTree(TreeNode root)
+    {
+        if (root == null)
+            return false;
+
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        int level = -1;
+        queue.Enqueue(root);
+
+        while(queue.Count > 0)
+        {
+            level++;
+            int preValue = 0;
+
+            for(int i = 0; i < queue.Count; i++)
+            {
+                TreeNode curr = queue.Dequeue();
+
+                if(level == 0 && curr.val % 2 == 0)
+                    return false;
+
+                if (i == 0)
+                {
+                    if ((level % 2 == 1 && curr.val % 2 == 0)
+                        || (level % 2 == 0 && curr.val % 2 == 1))
+                    {
+                        preValue = curr.val;
+                    } else
+                        return false;
+                }
+                else
+                {
+                    if(level % 2 == 0)
+                    {
+                        if(curr.val % 2 == 1 && preValue < curr.val)
+                            preValue = curr.val;
+                        else
+                            return false;
+                    }
+                    else
+                    {
+                        if (curr.val % 2 == 0 && preValue > curr.val)
+                            preValue = curr.val;
+                        else
+                            return false;
+                    }
+                }
+
+                if(curr.left != null)
+                    queue.Enqueue(curr.left);
+                
+                if(curr.right != null)
+                    queue.Enqueue(curr.right);
+            }
+        }
+        return true;
+    }
+}
