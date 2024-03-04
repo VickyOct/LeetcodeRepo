bfedece709b4237c9768c4175ed67e0c00f27988
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-03 19:15:12 -0500

    Create 977. Squares of the sorted array.cs

---
+++ Leetcode/LeetCode/LeetCode/977. Squares of the sorted array.cs
@@ -0,0 +1,13 @@
+﻿public class Solution
+{
+    public int[] SortedSquares(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            result[i] = nums[i] * nums[i];
+        }
+        Array.Sort(result);
+        return result;
+    }
+}
