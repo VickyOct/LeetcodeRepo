7c91be009671283d2837ff66d46d48859859c33c
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-03 20:07:38 -0500

    Create 948.BagOfTokens.cs

---
+++ Leetcode/LeetCode/948.BagOfTokens.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public int BagOfTokensScore(int[] tokens, int power)
+    {
+        Array.Sort(tokens);
+        int n = tokens.Length;
+        int left = 0;
+        int right = n - 1;
+        int score = 0;
+        int maxScore = 0;
+
+        while (left <= right)
+        {
+            if (tokens[left] <= power)
+            {
+                score++;
+                power -= tokens[left];
+                left++;
+            }
+            else if (score > 0)
+            {
+                score--;
+                power += tokens[right];
+                right--;
+            }
+            else
+            {
+                break;
+            }
+            maxScore = Math.Max(maxScore, score);
+
+        }
+        return maxScore;
+    }
+}
