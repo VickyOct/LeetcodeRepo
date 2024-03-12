56c3544ae4750388eab98af00ef319120343cedb
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-12 14:33:58 -0400

    Create 2540MinimumCommonValue.cs

---
+++ Leetcode/LeetCode/LeetCode/2540MinimumCommonValue.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int GetCommon(int[] nums1, int[] nums2)
+    {
+        HashSet<int> set = new HashSet<int>(nums1);
+        foreach (int i in nums2)
+        {
+            if (set.Contains(i))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
