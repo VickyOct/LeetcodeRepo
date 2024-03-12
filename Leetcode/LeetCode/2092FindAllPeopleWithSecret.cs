ae787a185382ad72bd42bbad7b4b635912c707c2
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-12 13:18:44 -0400

    Update 2092FindAllPeopleWithSecret.cs

--- Leetcode/LeetCode/2092FindAllPeopleWithSecret.cs
+++ Leetcode/LeetCode/2092FindAllPeopleWithSecret.cs
@@ -2,28 +2,65 @@
 {
     public IList<int> FindAllPeople(int n, int[][] meetings, int firstPerson)
     {
-        Array.Sort(meetings, (a, b) => {
-            int result = a[a.Length - 1].CompareTo(b[b.Length - 1]);
-            if (result == 0)
-            {
-                int newElement = a
-            }
-        });
-        
+        Array.Sort(meetings, (a, b) => a[2].CompareTo(b[2]));
+
         IList<int> peopleWithSecret = new List<int>();
         peopleWithSecret.Add(0);
         peopleWithSecret.Add(firstPerson);
+        int prev = 0;
 
-        for(int i = 0; i < meetings.Length; i++)
+        for (int i = 0; i < meetings.Length; i++)
         {
             var peopleWithSecretInMeeting = meetings[i].Take(2).Where(ppl => peopleWithSecret.Contains(ppl));
             if (peopleWithSecretInMeeting.Any())
             {
-                int peopleLearn = meetings[i].Take(2).FirstOrDefault(ppl => !peopleWithSecret.Contains(ppl));
-                if(peopleLearn <= n - 1 && !peopleWithSecret.Contains(peopleLearn))
-                    peopleWithSecret.Add(peopleLearn);
+                int personLearn = meetings[i].Take(2).FirstOrDefault(ppl => !peopleWithSecret.Contains(ppl));
+                if (personLearn >= 0 && personLearn < n && !peopleWithSecret.Contains(personLearn))
+                    peopleWithSecret.Add(personLearn);
+            }
+
+            if (prev != 0 && meetings[i][2] == meetings[prev][2])
+            {
+                IList<int> peopleInSameTime = new List<int>();
+                IList<int> peopleDontKnow = new List<int>();
+                bool secretWillShare = false;
+
+                foreach (int person in meetings[prev].Take(2))
+                {
+                    if (!peopleWithSecret.Contains(person) && !peopleDontKnow.Contains(person))
+                    {
+                        peopleDontKnow.Add(person);
+                    }
+                    if (peopleWithSecret.Contains(person))
+                    {
+                        secretWillShare = true;
+                    }
+                }
+
+                foreach (int person in meetings[i].Take(2))
+                {
+                    if (!peopleWithSecret.Contains(person) && !peopleDontKnow.Contains(person))
+                    {
+                        peopleDontKnow.Add(person);
+                    }
+                    if (peopleWithSecret.Contains(person))
+                    {
+                        secretWillShare = true;
+                    }
+                }
+
+                if (secretWillShare)
+                {
+                    foreach (int person in peopleDontKnow)
+                    {
+                        peopleWithSecret.Add(person);
+                    }
+                }
+
+                prev = i;
             }
         }
+
         return peopleWithSecret;
     }
 }
