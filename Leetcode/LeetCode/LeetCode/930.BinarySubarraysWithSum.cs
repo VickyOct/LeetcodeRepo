17733ad334e0e9208a081fe1c851cea9324c5ec1
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-14 11:44:20 -0400

    Create 930.BinarySubarraysWithSum.cs

---
+++ Leetcode/LeetCode/LeetCode/930.BinarySubarraysWithSum.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int NumSubarraysWithSum(int[] nums, int goal)
+    {
+        int count = 0;
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        map[0] = 1;
+        int sum = 0;
+
+        foreach (int num in nums)
+        {
+            sum += num;
+            int rem = sum - goal;
+            if (map.ContainsKey(rem))
+            {
+                count += map[rem];
+            }
+            if (!map.ContainsKey(sum))
+            {
+                map[sum] = 0;
+            }
+            map[sum]++;
+        }
+        return count;
+    }
+}
