c261e989daba102fe5623b8fe9e256656faa49c8
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-18 17:12:57 -0400

    Create 452. Minimum Number of Arrows to Burst Balloons.cs

---
+++ Leetcode/LeetCode/452. Minimum Number of Arrows to Burst Balloons.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int FindMinArrowShots(int[][] points)
+    {
+        Array.Sort(points, (a, b) => a[1].CompareTo(b[1]));
+
+        int arrows = 1;
+        int brustPower = points[0][1];
+
+        for(int i = 1; i < points.Length; i++)
+        {
+            if(points[i][0] <= brustPower)
+            {
+                continue;
+            }
+            arrows++;
+            brustPower = points[i][1];
+        }
+
+        return arrows;
+    }
+}
