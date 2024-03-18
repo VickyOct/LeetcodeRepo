fffa912c0b2b78916a86d53dd6772061ef0c3322
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-18 17:12:55 -0400

    Update 57. Insert Interval.cs

--- Leetcode/LeetCode/57. Insert Interval.cs
+++ Leetcode/LeetCode/57. Insert Interval.cs
@@ -2,7 +2,35 @@
 {
     public int[][] Insert(int[][] intervals, int[] newInterval)
     {
-        int pointer = 0;
+        List<int[]> result = new List<int[]>();
+        int start = newInterval[0];
+        int end = newInterval[1];
+        int size = intervals.Length;
+        int i = 0;
+
+        while(i < size && intervals[i][1] < start)
+        {
+            result.Add(intervals[i]);
+            i++;
+        }
+
+        while(i < size && intervals[i][0] <= end)
+        {
+            newInterval[0] = Math.Min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.Max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.Add(newInterval);
+
+        while(i < size)
+        {
+            result.Add(intervals[i]);
+            i++;
+        }
+
+        return result.ToArray();
+
+        /*int pointer = 0;
         int start = newInterval[0];
         int end = newInterval[1];
         int size = intervals.Length;
@@ -41,7 +69,7 @@
             i++;
         }
 
-            /*int size = intervals.Length;
+            int size = intervals.Length;
             int start = newInterval[0];
             int end = newInterval[1];
             List<int[]> result = new List<int[]>;
@@ -72,5 +100,5 @@
             }*/
 
 
-        }
+    }
 }
