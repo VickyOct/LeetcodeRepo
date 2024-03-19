8aaee9f6d733233a5e969baf12b0dc4cce97c3d8
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-19 15:31:31 -0400

    Create 621. Task Scheduler.cs
    Use int[] freq = new int[26];
    
            foreach (char c in tasks)
            {
                freq[c - 'A']++;
            } to get the frequency of char

---
+++ Leetcode/LeetCode/621. Task Scheduler.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int LeastInterval(char[] tasks, int n)
+    {
+        int[] freq = new int[26];
+
+        foreach (char c in tasks)
+        {
+            freq[c - 'A']++;
+        }
+
+        Array.Sort(freq);
+
+        int maxfreq = freq[25] - 1;
+        int idleSlots = maxfreq * n;
+
+        for (int i = 24; i>= 0 && freq[i] > 0; i--)
+        {
+            idleSlots -= Math.Min(freq[i], maxfreq);
+        }
+
+        return Math.Max(idleSlots, 0) + tasks.Length;
+    }
+}
