a471bba48754c15551b176a4b43fa4c7c0959cea
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-25 12:15:17 -0400

    Create 143. Reorder List.cs

---
+++ Leetcode/LeetCode/143. Reorder List.cs
@@ -0,0 +1,27 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public void ReorderList(ListNode head)
+    {
+        ListNode prefix = head;
+        ListNode suffix = head;
+       
+        ListNode result = null;
+
+        while(suffix.next.next != null)
+        {
+            prefix = prefix.next;
+            suffix = suffix.next.next;
+        }
+    }
+}
