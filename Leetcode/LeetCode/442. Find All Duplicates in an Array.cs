a1a05b0a16c0a0edcabeb72f3a37f4a8f4d3b75a
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-25 12:15:14 -0400

    Create 442. Find All Duplicates in an Array.cs

---
+++ Leetcode/LeetCode/442. Find All Duplicates in an Array.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public IList<int> FindDuplicates(int[] nums)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (map.ContainsKey(nums[i]))
+                map[nums[i]]++;
+            else
+                map[nums[i]] = 1;
+        }
+
+        List<int> result = map.Where(res => res.Value == 2).Select(res => res.Key).ToList();
+
+        return result;
+    }
+}
