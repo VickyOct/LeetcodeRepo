487d360b91f262051a24acd2ae97ace269a56736
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-25 10:47:26 -0400

    Create 287. Find the Duplicate Number.cs

---
+++ Leetcode/LeetCode/LeetCode/287. Find the Duplicate Number.cs
@@ -0,0 +1,14 @@
+﻿public class Solutiobn
+{
+    public int FindDuplicate(int[] nums)
+    {
+        Array.Sort(nums);
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] == nums[i + 1])
+            {
+                return nums[i];            
+            }
+        }
+    }
+}
