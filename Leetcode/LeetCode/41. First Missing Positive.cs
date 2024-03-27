0a9779072f8d71a76c18e548c3cd6b9a85dbec84
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-27 09:22:31 -0400

    Create 41. First Missing Positive.cs

---
+++ Leetcode/LeetCode/41. First Missing Positive.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public int FirstMissingPositive(int[] nums)
+    {
+        Array.Sort(nums);
+        int n = nums.Length;
+        int pos = 0;
+        int positiveInt = 1;
+
+        while (nums[pos] <= 0)
+        {
+            pos++;
+            if (pos >= n)
+                return 1;
+        }
+
+
+        for (int i = pos; i < n; i++)
+        {
+            if (nums[i] == positiveInt)
+            {
+                positiveInt++;
+            }
+            else if (i > pos && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            else
+            {
+                return positiveInt;
+            }
+        }
+        return positiveInt;
+    }
+}
