72d793a0226f573311b7b41f94ec1e126479e572
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-27 11:56:28 -0400

    Create 713.Subarray Product Less Than K.cs

---
+++ Leetcode/LeetCode/713.Subarray Product Less Than K.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int NumSubarrayProductLessThanK(int[] nums, int k)
+    {
+        int count = 0;
+        for(int left = 0; left < nums.Length; left++)
+        {
+            int right = left;
+            int sum = nums[left];
+
+            while(sum < k)
+            {
+                count++;
+                right++;
+                if (right < nums.Length)
+                {
+                    sum *= nums[right];
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
