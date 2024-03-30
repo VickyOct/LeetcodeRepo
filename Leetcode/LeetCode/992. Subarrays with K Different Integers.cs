6d95ee3a4c6125486e201940ae8aed13ffd0d2a9
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-30 15:47:31 -0400

    Create 992. Subarrays with K Different Integers.cs
    First try but extend time limit

---
+++ Leetcode/LeetCode/992. Subarrays with K Different Integers.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public int SubarraysWithKDistinct(int[] nums, int k)
+    {
+        int result = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            List<int> numsExit = new List<int>();
+
+            for (int pointer = i; pointer < nums.Length; pointer++)
+            {
+                if (!numsExit.Contains(nums[pointer]))
+                {
+                    numsExit.Add(nums[pointer]);
+                    if (numsExit.Count > k)
+                        break;
+
+                    if (numsExit.Count == k)
+                        result++;
+                }
+                else if (numsExit.Contains(nums[pointer]) && numsExit.Count == k)
+                {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
