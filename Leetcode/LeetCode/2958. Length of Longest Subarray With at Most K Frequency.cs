ecefcaadd78872d20d7949a1fa87d1b3fbfe6600
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-30 15:47:08 -0400

    Create 2958. Length of Longest Subarray With at Most K Frequency.cs

---
+++ Leetcode/LeetCode/2958. Length of Longest Subarray With at Most K Frequency.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public int MaxSubarrayLength(int[] nums, int k)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        int maxLength = 0;
+        int repeatPointer = 0;
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if (map.ContainsKey(nums[i]))
+            {
+                map[nums[i]]++;
+                if (map[nums[i]] > k)
+                {
+                    maxLength = Math.Max(i - repeatPointer, maxLength);
+                    repeatPointer = i;
+                }
+            }
+            else
+                map[nums[i]] = 1;
+        }
+
+        if(maxLength != 0)
+        {
+            return maxLength;
+        }
+        else
+        {
+            return nums.Length;
+        }
+    }
+}
