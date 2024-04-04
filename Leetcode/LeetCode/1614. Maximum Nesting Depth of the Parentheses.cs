49365c2747e70a9d6e77f7592de3a62a5fcd8c4a
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-04-04 11:29:32 -0400

    Create 1614. Maximum Nesting Depth of the Parentheses.cs

---
+++ Leetcode/LeetCode/1614. Maximum Nesting Depth of the Parentheses.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int MaxDepth(string s)
+    {
+        int count = 0;
+        int Max = 0;
+        foreach (char c in s)
+        {
+            if (c == '(')
+            {
+                count++;
+                if (count > Max)
+                    Max = count;
+            }
+            else if (c == ')')
+                count--;
+        }
+        return Max;
+    }
+}
