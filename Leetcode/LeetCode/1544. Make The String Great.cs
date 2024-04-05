c2eaa1d0a4b7bfbbe14efefcb6a2ddd7a6e4fab1
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-04-05 11:37:58 -0400

    Create 1544. Make The String Great.cs

---
+++ Leetcode/LeetCode/1544. Make The String Great.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public string MakeGood(string s)
+    {
+        StringBuilder SB = new StringBuilder();
+
+        foreach (char c in s)
+        {
+            if(SB.Length > 0 && Math.Abs(c - SB[SB.Length - 1]) == 32)
+            {
+                SB.Length--;
+            }
+            else
+            {
+                SB.Append(c);
+            }
+        }
+        return SB.ToString();
+    }
+}
