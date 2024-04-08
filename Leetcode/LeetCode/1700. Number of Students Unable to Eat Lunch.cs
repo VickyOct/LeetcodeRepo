4e60beb21310a1e93c325356307e7701b78c227c
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-04-07 22:21:23 -0400

    Create 1700. Number of Students Unable to Eat Lunch.cs

---
+++ Leetcode/LeetCode/1700. Number of Students Unable to Eat Lunch.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int CountStudents(int[] students, int[] sandwiches)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>()
+        {
+            {0, 0},
+            {1, 0}
+        };
+
+        foreach(int student in students)
+        {
+            if(map.ContainsKey(student))
+                map[student]++;
+        }
+
+        foreach(int saw in sandwiches)
+        {
+            if (map[saw] == 0)
+                break;
+            map[saw]--;
+        }
+
+        return map.Values.Sum();
+    }
+}
