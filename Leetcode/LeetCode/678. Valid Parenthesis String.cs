7eb7c48062ef00848c692a4a112eb51b40d7ea6d
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-04-08 12:14:20 -0400

    Create 678. Valid Parenthesis String.cs

---
+++ Leetcode/LeetCode/678. Valid Parenthesis String.cs
@@ -0,0 +1,38 @@
+﻿public class Solution
+{
+    public bool CheckValidString(string s)
+    {
+        int Min = 0;
+        int Max = 0;
+
+        foreach (char c in s)
+        {
+            if (c == '(')
+            {
+                Min++;
+                Max++;
+            }
+            else if (c == ')')
+            {
+                Min--;
+                Max--;
+            }
+            else if (c == '*')
+            {
+                Min--;
+                Max++;
+            }
+
+            if(Max < 0)
+                return false;
+
+            if(Min < 0)
+                Min = 0;
+        }
+
+        if (Min == 0)
+            return true;
+
+        return false;
+    }
+}
