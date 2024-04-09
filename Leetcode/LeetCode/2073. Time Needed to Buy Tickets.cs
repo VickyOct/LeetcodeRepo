035d60ede117f1aafb389cdee0acec7660ad1e4f
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-04-09 10:40:55 -0400

    Create 2073. Time Needed to Buy Tickets.cs

---
+++ Leetcode/LeetCode/2073. Time Needed to Buy Tickets.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int TimeRequiredToBuy(int[] tickets, int k)
+    {
+        int second = 0;
+
+        for(int i = 0; i < tickets.Length; i++)
+        {
+            tickets[i]--;
+            second++;
+
+            if (tickets[k] == 0)
+                break;
+
+            if (tickets[i] < 0)
+            {
+                second--;
+            }
+
+                if (i == tickets.Length - 1 && tickets[k] != 0)
+            {
+                i = -1;
+            }
+        }
+        return second;
+    }
+}
