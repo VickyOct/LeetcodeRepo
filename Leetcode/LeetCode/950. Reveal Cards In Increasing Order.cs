7cffc5d459f6d1d0328ee5df58595766e7440415
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-04-10 12:40:30 -0400

    Create 950. Reveal Cards In Increasing Order.cs

---
+++ Leetcode/LeetCode/950. Reveal Cards In Increasing Order.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public int[] DeckRevealedIncreasing(int[] deck)
+    {
+        Array.Sort(deck);
+        Queue<int> indexQueue = new Queue<int>(Enumerable.Range(0, deck.Length));
+        int[] result = new int[deck.Length];
+
+        foreach (int card in deck)
+        {
+            result[indexQueue.Dequeue()] = card;
+            if(indexQueue.Count > 0)
+                indexQueue.Enqueue(indexQueue.Dequeue());
+        }
+        return result;
+    }
+}
