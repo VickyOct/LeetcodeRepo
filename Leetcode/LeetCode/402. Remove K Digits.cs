1a7802eb85de8cf1b4910d1e67aad55e8c6860ca
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-04-11 12:03:24 -0400

    Create 402. Remove K Digits.cs

---
+++ Leetcode/LeetCode/402. Remove K Digits.cs
@@ -0,0 +1,36 @@
+﻿public class Solution
+{
+    public string RemoveKdigits(string num, int k)
+    {
+        int n = num.Length;
+        if (k == n)
+            return "0";
+
+        Stack<char> st = new Stack<char>();
+        for(int i = 0; i < n; i++)
+        {
+            char digit = num[i];
+            while(st.Count > 0 && k > 0 && st.Peek() > digit)
+            {
+                st.Pop();
+                k--;
+            } 
+
+            st.Push(digit);
+        }
+
+        while(k > 0)
+        {
+            st.Pop();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (st.Count > 0)
+            sb.Insert(0, st.Pop());
+
+        sb.Replace("0", "").ToString();
+
+        return sb.Length == 0? "0" : sb.ToString();
+    }
+}
