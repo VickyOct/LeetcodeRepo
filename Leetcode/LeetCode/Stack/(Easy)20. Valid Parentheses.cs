ad6475a2368f901a34629a5f4c889abe9593d067
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-01 15:58:16 -0400

    Create (Easy)20. Valid Parentheses.cs

---
+++ Leetcode/LeetCode/Stack/(Easy)20. Valid Parentheses.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> brackets = new Stack<char>();
+
+        foreach (char c in s)
+        {
+            if (c == '(')
+                brackets.Push(')');
+            else if (c == '{')
+                brackets.Push('}');
+            else if (c == '[')
+                brackets.Push(']');
+            else if (brackets.Count == 0 || brackets.Pop() != c)
+                return false;
+        }
+        return brackets.Count == 0;
+    }
+}
