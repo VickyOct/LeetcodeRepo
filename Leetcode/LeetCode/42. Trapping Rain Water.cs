6c385ddb19ff0e68eab67a9a27a7419a2e93da51
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-01 15:58:10 -0400

    Create 42. Trapping Rain Water.cs

---
+++ Leetcode/LeetCode/42. Trapping Rain Water.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int Trap(int[] height)
+    {
+        int left = 0;
+        int right = 0;
+
+        for(int i = 0; i < height.Length; i++)
+        {
+            if(height[i] >= left )
+                left = height[i];
+
+            if(left < right)
+        }
+    }
+}
