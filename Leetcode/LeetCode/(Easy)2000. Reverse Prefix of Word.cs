ab8542c50ae3622e6c97cc7112aaeb1b10925d8d
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-01 16:42:55 -0400

    Create (Easy)2000. Reverse Prefix of Word.cs

---
+++ Leetcode/LeetCode/(Easy)2000. Reverse Prefix of Word.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public string ReversePrefix(string word, char ch)
+    {
+        if(!word.Contains(ch))
+            return word;
+
+        StringBuilder sb = new StringBuilder(word);
+        int index = word.IndexOf(ch);
+        int l = 0;
+
+        while(l < index)
+        {
+            (sb[l], sb[index]) = (sb[index], sb[l]);
+            l++;
+            index--;
+        }
+        return sb.ToString();
+    }
+}
