fc28d2fd0df966abc7ae662e5101ffcff9d2edcd
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-01 15:58:14 -0400

    Create 85. Maximal Rectangle.cs

---
+++ Leetcode/LeetCode/85. Maximal Rectangle.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int MaximalRectangle(char[][] matrix)
+    {
+        int rows = matrix.Length;
+        int cols = matrix[0].Length;
+        int[] heights = new int[cols];
+        int maxArea = 0;
+
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                heights[j] = matrix[i][j] == '1'? heights[j] + 1 : 0;
+            }            
+            
+        }
+    }
+}
