afbb4ee21ec371cbd3ec185bcda309366c8f6ad2
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-02 10:24:23 -0400

    Create (easy)2441. Largest Positive Integer That Exists With Its Negative.cs

---
+++ Leetcode/LeetCode/n/(easy)2441. Largest Positive Integer That Exists With Its Negative.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int FindMaxK(int[] nums)
+    {
+        Array.Sort(nums);
+        int j = nums.Length - 1, i = 0;
+        while (i < j)
+        {
+            int a = -1 * nums[j];
+            if (nums[i] == a)
+                return nums[j];
+            else if (nums[i] < a)
+                i++;
+            else
+                j--;
+        }
+        return -1;
+    }
+}
