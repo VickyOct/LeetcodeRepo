e97abd12b515f53cdf28a724e3964c09d740d566
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-06 14:57:32 -0400

    Create M2487. Remove Nodes From Linked List.cs

---
+++ Leetcode/LeetCode/n/listNode/M2487. Remove Nodes From Linked List.cs
@@ -0,0 +1,38 @@
+﻿public class Solution
+{
+    public ListNode RemoveNodes(ListNode head)
+    {
+        ListNode dummy = head;
+        ListNode rev = null;
+
+        while(dummy != null)
+        {
+            ListNode temp = dummy.next;
+            dummy.next = rev;
+            rev = dummy;
+            dummy = temp;
+        }
+
+
+        ListNode NewList = null;
+        int MaxValue = int.MinValue;
+
+        while(rev != null)
+        {
+            if(rev.val >= MaxValue)
+            {
+                MaxValue = rev.val;
+                ListNode temp = rev.next;
+                rev.next = NewList;
+                NewList = rev;
+                rev = temp;
+            }
+            else
+            {
+                rev = rev.next;
+            }
+        }
+
+        return NewList;
+    }
+}
