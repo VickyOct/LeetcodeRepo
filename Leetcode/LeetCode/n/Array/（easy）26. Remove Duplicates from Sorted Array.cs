afde778e7a9c0a4da8aa07c118c8332f0432e736
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-14 13:04:22 -0400

    Create （easy）26. Remove Duplicates from Sorted Array.cs

---
+++ Leetcode/LeetCode/n/Array/（easy）26. Remove Duplicates from Sorted Array.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return 0;
+        }
+
+        int count = 0;
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if (!map.ContainsKey(nums[i]))
+            {
+                map[nums[i]] = 1;
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
