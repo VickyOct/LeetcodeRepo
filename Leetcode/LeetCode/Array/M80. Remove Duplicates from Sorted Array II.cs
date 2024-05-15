8578e83aa57f609b060c3d74c9510c5cfece62ea
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-15 19:35:43 -0400

    Create M80. Remove Duplicates from Sorted Array II.cs

---
+++ Leetcode/LeetCode/Array/M80. Remove Duplicates from Sorted Array II.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int count = 0;
+
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!map.ContainsKey(nums[i]))
+            {
+                map[nums[i]] = 1;
+            }
+            else
+                map[nums[i]]++;
+
+            if (map[nums[i]] < 3)
+            {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
