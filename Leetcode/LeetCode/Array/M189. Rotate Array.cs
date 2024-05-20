fea3259db11dd4f493b9983b3a467365b6e8b2cc
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-19 21:13:53 -0400

    Create M189. Rotate Array.cs

---
+++ Leetcode/LeetCode/Array/M189. Rotate Array.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        int n = nums.Length;
+        k %= n;
+
+        Reverse(nums, 0, n - 1);
+        Reverse(nums, 0, k - 1);
+        Reverse(nums, k, n - 1);
+    }
+
+    private void Reverse(int[] arr, int start, int end)
+    {
+        while (start < end)
+        {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
