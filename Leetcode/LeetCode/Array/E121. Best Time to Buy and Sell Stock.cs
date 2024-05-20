0d6aeb2e5ddf64c15466076cc1533269157047b5
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-19 21:13:50 -0400

    Solve 121. Best Time to Buy and Sell Stock

---
+++ Leetcode/LeetCode/Array/E121. Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int max = 0;
+        int min = prices[0];
+
+        for(int i = 1; i < prices.Length; i++)
+        {
+            if(prices[i] > min)
+                max = Math.Max(max, prices[i] - min);
+
+            if (prices[i] < min)
+                min = prices[i];
+        }
+        return max;
+    }
+}
