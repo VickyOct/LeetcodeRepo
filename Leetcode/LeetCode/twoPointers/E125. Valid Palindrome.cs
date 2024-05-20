80c85a6b8a6dad455e73974ce136672c5485975f
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-20 17:17:29 -0400

    Create E125. Valid Palindrome.cs

---
+++ Leetcode/LeetCode/twoPointers/E125. Valid Palindrome.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        s = s.ToLower();
+        s = new string(s.Where(c => char.IsLetterOrDigit(c)).ToArray());
+        if (s.Length < 1)
+            return true;
+
+        int left = 0;
+        int right = s.Length -1;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if(s[left] != s[right])
+                return false;
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
