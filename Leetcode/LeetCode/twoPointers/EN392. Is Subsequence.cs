ecaa31478c5becaa9f040ab0d0a1fbd2ec22f9d4
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-20 17:20:45 -0400

    Create EN392. Is Subsequence.cs

---
+++ Leetcode/LeetCode/twoPointers/EN392. Is Subsequence.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if (t.Length == 0 && s.Length != 0)
+            return false;
+
+        int sPointer = 0;
+        int tPointer = 0;
+
+        while (sPointer < s.Length)
+        {
+            if (s[sPointer] == t[tPointer])
+            {
+                sPointer++;
+
+                if (tPointer < t.Length - 1)
+                    tPointer++;
+                else
+                    return false;
+
+            }
+            else if (tPointer == t.Length - 1 && s[sPointer] != s.Length)
+            {
+                return false;
+            }
+            else
+            {
+                tPointer++;
+            }
+        }
+        return true;
+    }
+}
