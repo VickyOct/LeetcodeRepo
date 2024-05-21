05ef58d5e8a864e8c8ba6a72cd28560bdf176844
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-20 23:06:59 -0400

    Create M11. Container With Most Water.cs

---
+++ Leetcode/LeetCode/twoPointers/M11. Container With Most Water.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        int left = 0;
+        int right = height.Length - 1;
+        int maxArea = 0;
+
+        while(left < right)
+        {
+            if(height[left] < height[right])
+            {
+                maxArea = Math.Max(maxArea, height[left] * (right - left));
+                left++;
+            }else
+            {
+                maxArea = Math.Max(maxArea, height[right] * (right - left));
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
