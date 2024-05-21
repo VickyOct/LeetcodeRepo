adaebb0054d0b2a604a89f951ecf0f71d0c0c6b6
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-20 23:07:02 -0400

    Create MN167. Two Sum II - Input Array Is Sorted.cs

---
+++ Leetcode/LeetCode/twoPointers/MN167. Two Sum II - Input Array Is Sorted.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        int left = 0;
+        int right = numbers.Length - 1;
+
+        while (left < right)
+        {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target)
+            {
+                return new int[] { left + 1, right + 1 }; // +1 because the problem expects 1-based indices
+            }
+            else if (sum < target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return new int[2]; // This should not be reached if there's always a valid solution
+    }
+}
