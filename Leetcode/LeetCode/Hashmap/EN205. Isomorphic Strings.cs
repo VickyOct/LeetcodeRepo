3b0bff514e5091bd01189c597cbcd3f5c30ac591
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-22 13:20:46 -0400

    Create EN205. Isomorphic Strings.cs

---
+++ Leetcode/LeetCode/Hashmap/EN205. Isomorphic Strings.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        if(s.Length != t.Length)
+            return false;
+
+        Dictionary<char, char> map = new Dictionary<char, char>();
+        HashSet<char> tSet = new HashSet<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            char sChar = s[i];
+            char tChar = t[i];
+            if (!map.ContainsKey(sChar))
+            {
+                if(tSet.Contains(tChar))
+                    return false;
+            }
+            else
+            {
+                if(tChar != map[sChar])
+                    return false;
+            }
+
+            map[sChar] = tChar;
+            tSet.Add(tChar);
+        }
+        return true;
+    }
+}
