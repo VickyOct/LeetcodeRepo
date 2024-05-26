35e5a7ee42efa01d6fe1777009173d4dc05326df
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-26 15:36:21 -0400

    Create EN228. Summary Ranges.cs

---
+++ Leetcode/LeetCode/intervals/EN228. Summary Ranges.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public IList<string> SummaryRanges(int[] nums)
+    {
+        List<string> result = new List<string>();
+
+        if (nums.Length == 0)
+            return result;
+
+        int start = 0;
+
+        for (int i = 1; i <= nums.Length; i++)
+        {
+            if (i == nums.Length || nums[i] != nums[i - 1] + 1)
+            {
+                if (start == i - 1)
+                {
+                    result.Add(nums[start].ToString());
+                }
+                else
+                {
+                    result.Add($"{nums[start]}->{nums[i - 1]}");
+                }
+                start = i;
+            }
+        }
+
+        return result;
+    }
+}
