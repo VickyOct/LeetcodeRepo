f6f40aeaa28dbf2b7e7eadaec8f99254921d0894
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-05-26 15:36:28 -0400

    Create MN56. Merge Intervals.cs

---
+++ Leetcode/LeetCode/intervals/MN56. Merge Intervals.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public int[][] Merge(int[][] intervals)
+    {
+        if (intervals.Length == 1)
+            return intervals;
+
+        List<int[]> result = new List<int[]>();
+        int start = 0;
+        for (int i = 1; i < intervals.Length; i++)
+        {
+            if (i <= intervals.Length - 1
+                && intervals[start][1] >= intervals[i][0])
+            {
+                if (intervals[start][1] < intervals[i][1])
+                {
+                    result.Add(new int[] { intervals[start][0], intervals[i][1] });
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                result.Add(intervals[i]);
+            }
+            start = i;
+        }
+        return result.ToArray();
+    }
+}
